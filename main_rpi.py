822918c869ce2dec52075cdc70da6b15e00b93e7
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2022-03-13 01:18:51 -0300

    add: the software now create logs

--- main_rpi.py
+++ main_rpi.py
@@ -1,3 +1,4 @@
+from venv import create
 from pydub.playback import play
 from pydub import AudioSegment
 from os import path
@@ -15,11 +16,12 @@ def getScriptFolder():
     return scriptPath[0]
 
 def enablePins(pin):
-    gpio.setmode(gpio.BOARD)
+    createLog(1, """Porta {pin} habilitada""")
     gpio.setup(pin, gpio.OUT)
     gpio.output(pin, gpio.HIGH)
 
 def disablePins(pin):
+    createLog(1, """Porta {pin} desabilitada""")
     gpio.output(pin, gpio.LOW)
     gpio.cleanup()
 
@@ -36,10 +38,12 @@ def connectMysql():
     )
 
     if (mysql):
-        print('[INFO] [{time}] Sucesso na conexão ao banco de dados')
+        createLog(1, 'Database connection success!')
         mycursor = mydb.cursor()
+    else:
+        createLog(3, 'Error on DB connection.')
 
-def createLog(case):
+def createLog(case, message):
     logging.basicConfig(
     filename='pythonAlarm.log',
     format='[%(levelname)s] - %(asctime)s - %(message)s',
@@ -48,30 +52,45 @@ def createLog(case):
     level=logging.DEBUG
     )
 
+    switch = {
+        1: logging.info(message),
+        2: logging.warning(message),
+        3: logging.error(message)
+    }
 
+    switch.get(case, "Opção inválida")
+
+#Script Folder
+ROOT_FOLDER = getScriptFolder()
+#Webfiles folder
+WEB_FOLDER = "/web/"
+#SQL View - I used this method because when I imported the db from docker it broke the view, so I decided to use this. I think it is not elegant but it works.
+VIEW_SQL = "SELECT a.name as nomeAlarme, a.startTime as horaDoAlarme, aa.Nome as area, aa.GPIO_PORT as porta, also.songTime as tempoMusica, also.fadeinTime as fadeIn, also.fadeoutTime as fadeout, s.path as path FROM Alarm as a INNER JOIN AlarmArea as aa ON aa.ID = a.AlarmeAreaID INNER JOIN AlarmSong as also ON also.alarmID = a.alarmID INNER JOIN Song as s ON s.songID = also.songID HAVING "
+
+# Starting RPi Ports
+gpio.setmode(gpio.BOARD)
+
+# Starting DB vars
 mydb = 0
 mycursor = 0
-ROOT_FOLDER = getScriptFolder()
-WEB_FOLDER = "/web/"
-VIEW_SQL = "SELECT a.name as nomeAlarme, a.startTime as horaDoAlarme, aa.Nome as area, aa.GPIO_PORT as porta, also.songTime as tempoMusica, also.fadeinTime as fadeIn, also.fadeoutTime as fadeout, s.path as path FROM Alarm as a INNER JOIN AlarmArea as aa ON aa.ID = a.AlarmeAreaID INNER JOIN AlarmSong as also ON also.alarmID = a.alarmID INNER JOIN Song as s ON s.songID = also.songID HAVING "
 
 connectMysql()
 
+# Get time to search on DB
 hora = getTime()
 hora = hora.strftime("%H:%M")
 
+# Creating SQL instruction
 sql = VIEW_SQL + ("horaDoAlarme='{hora}'".format(hora=hora))
-print('[INFO] [{hora}] Realizando pesquisa no banco de dados')
+createLog(1, 'Realizando pesquisa no banco de dados')
 
 mycursor.execute(sql)
 aux=mycursor.fetchall()
 
-
 rc = mycursor.rowcount
 
 if (rc>=1):
-    hora = getTime()
-    print('[INFO] [{hora}] Processando dados do alarme')
+    createLog(1, 'Processando dados do alarme')
     nome_alarme = aux[0][0]
     arquivo = aux[0][7] #listar aqui o local do arquivo
     song_time = aux[0][4]*1000 #tempo da música
@@ -81,17 +100,15 @@ if (rc>=1):
     porta = aux[0][3]
 
     pathSong = ROOT_FOLDER + WEB_FOLDER + arquivo
-    hora = getTime()
-    print('[INFO] [{hora}] Preparando música para reprodução')
+    createLog(1, 'Preparando música para reprodução')
     song = AudioSegment.from_mp3(pathSong)
-    hora = getTime()
-    print("[EXEC] [{hora}] Há um alarme sendo executado agora")
-    print("""[INFO] Dados do alarme
-        Alarm: {nameAlarm}
-        Song: {songName}
+    createLog(1, 'Há um alarme sendo executado agora')
+    createLog(1,"""[INFO] Dados do alarme
+        Alarm: {nome_alarme}
+        Song: {arquivo}
         Path: {pathSong}
-        Time: {timeAlarm}
-    """.format(nameAlarm = nome_alarme, songName = arquivo, pathSong = pathSong, timeAlarm = song_time))
+        Time: {song_time}
+    """ )
     enablePins(porta)
     cut_song = song[:song_time]
     cut_song_with_effect = cut_song.fade_in(fadein_time).fade_out(fadeout_time)
