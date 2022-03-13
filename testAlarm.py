0121770238548a6cd0fe629162bc8a2c4e011048
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2022-03-13 00:07:50 -0300

    fix: print scripts wasn't working

--- testAlarm.py
+++ testAlarm.py
@@ -8,7 +8,8 @@ import datetime
 print ("Olá, Esse é o Teste de Alarme do PythonAlarm, será usado para listar os alarmes e testar a velocidade de reprodução das músicas.")
 
 def getTime():
-    return datetime.datetime.now()
+    horaAux = datetime.datetime.now()
+    return horaAux
 
 def getScriptFolder():
     scriptPath = path.split(path.realpath(__file__))
@@ -36,7 +37,7 @@ def connectMysql():
     )
 
     if (mysql):
-        print('[INFO] [{time}] Sucesso na conexão ao banco de dados')
+        print(f'[INFO] [{time}] Sucesso na conexão ao banco de dados')
         mycursor = mydb.cursor()
 
 mydb = 0
@@ -49,7 +50,7 @@ connectMysql()
 hora = getTime()
 
 sql = VIEW_SQL
-print('[INFO] [{hora}] Realizando pesquisa no banco de dados')
+print(f'[INFO] [{hora}] Realizando pesquisa no banco de dados')
 
 mycursor.execute(sql)
 result=mycursor.fetchall()
@@ -57,12 +58,12 @@ result=mycursor.fetchall()
 rc = mycursor.rowcount
 
 hora = getTime()
-print('[INFO] [{hora}] Listando dados dos alarmes cadastrados')
+print(f'[INFO] [{hora}] Listando dados dos alarmes cadastrados')
 
-i=0
+i=1
 
 if(rc>0):    
-    for(aux in result):
+    for aux in result:
         nome_alarme = aux[0]
         arquivo = aux[7] #listar aqui o local do arquivo
         song_time = aux[4]*1000 #tempo da música
@@ -71,20 +72,22 @@ if(rc>0):
         area = aux[2]
         porta = aux[3]
 
-        print("Numero: {i}")
-        print("Alarme: {nome_alarme}")
-        print("Arquivo: {arquivo}")
-        print("Tempo da Musica: {song_time}")
-        print("Fade In: {fadein_time}")
-        print("Fade Out: {nome_alarme}")
-        print("Area de execução: {area}")
-        print("GPIO: {porta}")
+        print(f"Numero: {i}")
+        print(f"Alarme: {nome_alarme}")
+        print(f"Arquivo: {arquivo}")
+        print(f"Tempo da Musica: {song_time}")
+        print(f"Fade In: {fadein_time}")
+        print(f"Fade Out: {nome_alarme}")
+        print(f"Area de execução: {area}")
+        print(f"GPIO: {porta}")
         print("-----------------------------")
+        i+=1
 
-    op = input("Escolha uma opção para executar: ")
+    op = int(input("Escolha uma opção para executar: "))
 
-    if(op>=result.length):
-        print('[INFO] [{hora}] Processando dados do alarme')
+    if(op>0):
+        print(f'[INFO] [{hora}] Processando dados do alarme')
+        op-=1
         nome_alarme = result[op][0]
         arquivo = result[op][7] #listar aqui o local do arquivo
         song_time = result[op][4]*1000 #tempo da música
@@ -95,10 +98,10 @@ if(rc>0):
 
         pathSong = ROOT_FOLDER + WEB_FOLDER + arquivo
         hora = getTime()
-        print('[INFO] [{hora}] Preparando música para reprodução')
+        print(f'[INFO] [{hora}] Preparando música para reprodução')
         song = AudioSegment.from_mp3(pathSong)
         hora = getTime()
-        print("[EXEC] [{hora}] Há um alarme sendo executado agora")
+        print(f"[EXEC] [{hora}] Há um alarme sendo executado agora")
         print("""[INFO] Dados do alarme
             Alarm: {nameAlarm}
             Song: {songName}
@@ -110,5 +113,7 @@ if(rc>0):
         cut_song_with_effect = cut_song.fade_in(fadein_time).fade_out(fadeout_time)
         play(cut_song_with_effect)
         disablePins(porta)
+    else:
+        print("[INFO] você escolheu uma opção inválida")
 else:
     print("[INFO] Não há alarmes cadastrados no sistema")
